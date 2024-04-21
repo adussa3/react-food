d0f91a3b3b81f9e0d6b93ac4e780763b704a0941
Anunoy Dussa <adussa3@gmail.com>
2024-04-20 22:49:34 -0700

    Updated backend and added server-side validation checks

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import fs from 'node:fs/promises';
 
 import bodyParser from 'body-parser';
 import express from 'express';
+import { nanoid } from 'nanoid';
 
 const app = express();
 
@@ -23,33 +24,51 @@ app.get('/meals', async (req, res) => {
 app.post('/orders', async (req, res) => {
   const orderData = req.body.order;
 
-  if (orderData === null || orderData.items === null || orderData.items === []) {
+  if (orderData === null || orderData.items === null || orderData.items == []) {
     return res
       .status(400)
       .json({ message: 'Missing data.' });
   }
 
-  if (
-    orderData.customer.email === null ||
-    !orderData.customer.email.includes('@') ||
-    orderData.customer.name === null ||
-    orderData.customer.name.trim() === '' ||
-    orderData.customer.street === null ||
-    orderData.customer.street.trim() === '' ||
-    orderData.customer['postal-code'] === null ||
-    orderData.customer['postal-code'].trim() === '' ||
-    orderData.customer.city === null ||
-    orderData.customer.city.trim() === ''
-  ) {
+  if (orderData.customer.email === null || !orderData.customer.email.includes('@')) {
     return res.status(400).json({
-      message:
-        'Missing data: Email, name, street, postal code or city is missing.',
+      message: "Missing the required email data"
+    });
+  }
+
+  if (orderData.customer.name === null || orderData.customer.name.trim() === '') {
+    return res.status(400).json({
+      message: "Missing the required name data"
+    });
+  }
+
+  if (orderData.customer.street === null || orderData.customer.street.trim() === '') {
+    return res.status(400).json({
+      message: "Missing the required street data"
+    });
+  }
+
+  if (orderData.customer.city === null || orderData.customer.city.trim() === '') {
+    return res.status(400).json({
+      message: "Missing the required city data"
+    });
+  }
+
+  if (orderData.customer.state === null || orderData.customer.state.trim() === '') {
+    return res.status(400).json({
+      message: "Missing the required state data"
+    });
+  }
+
+  if (orderData.customer['postal-code'] === null || orderData.customer['postal-code'].trim() === '') {
+    return res.status(400).json({
+      message: "Missing the required postal code data"
     });
   }
 
   const newOrder = {
     ...orderData,
-    id: (Math.random() * 1000).toString(),
+    id: nanoid(),
   };
   const orders = await fs.readFile('./data/orders.json', 'utf8');
   const allOrders = JSON.parse(orders);
