0c236fac3ca853e4a58629ca4ca42f075489bb6e
adussa3 <adussa3@gmail.com>
2024-04-24 12:56:49 -0700

    Updated the HTTP Request by removing "http://localhost:3000"

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -9,7 +9,7 @@ export default function MealItem({ meal }) {
 
     return (
         <li className="meal-item">
-            <img src={`http://localhost:3000/${meal.image}`} alt="" />
+            <img src={`/${meal.image}`} alt="" />
             <h3>{meal.name}</h3>
             <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
             <p className="meal-item-description">{meal.description}</p>
--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchMeals() {
     try {
-        const response = await axios.get("http://localhost:3000/meals");
+        const response = await axios.get("/meals");
         return response.data;
     } catch (error) {
         const message = `Failed to fetch meals data! \n\n ${error.message}`;
@@ -12,7 +12,7 @@ export async function fetchMeals() {
 
 export async function updateOrders(orderData) {
     try {
-        const response = await axios.post("http://localhost:3000/orders", orderData);
+        const response = await axios.post("/orders", orderData);
         return response;
     } catch (error) {
         const message = `Failed to update order data \n\n ${error.response.data.message} \n\n ${error.message}`;
